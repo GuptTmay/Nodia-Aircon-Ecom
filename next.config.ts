c4c294cdb4e1dd61d517e60bd9c55967c25f6288
GuptTmay <tanmaygupta22003@gmail.com>
2025-12-21 21:49:02 +0530

    removing nextjs non prod config

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverSourceMaps: false,
-    preloadEntriesOnStart: false,
-  },
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: false,
-  },
-  productionBrowserSourceMaps: false,
+  // experimental: {
+  //   serverSourceMaps: false,
+  //   preloadEntriesOnStart: false,
+  // },
+  // typescript: {
+  //   // !! WARN !!
+  //   // Dangerously allow production builds to successfully complete even if
+  //   // your project has type errors.
+  //   // !! WARN !!
+  //   ignoreBuildErrors: false,
+  // },
+  // productionBrowserSourceMaps: false,
   images: {
     remotePatterns: [new URL('https://picsum.photos/**')],
   },
