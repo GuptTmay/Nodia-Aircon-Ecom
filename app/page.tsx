ad96513ccdd9b7b01f8c56a4f8f0bc329bd76691
GuptTmay <tanmaygupta22003@gmail.com>
2025-12-21 21:52:41 +0530

    main page is static

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import prisma from '@/lib/prisma'
 import { ProductCard } from '@/components/ProductCard'
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   // const products = await prisma.product.findMany()
